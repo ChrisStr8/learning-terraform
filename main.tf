2e61a064593c9af389210ededdac64bbf612380c
Chris Straight <christopher.straight@fdmgroup.com>
2024-05-08 17:04:32 +1000

    module for security group

--- main.tf
+++ main.tf
@@ -22,11 +22,25 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.2"
+  name = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules = ["htp-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog"{
@@ -37,20 +51,20 @@ resource "aws_security_group" "blog"{
 }
 
 resource "aws_security_group_rule" "blog_http_in"{
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_http_out"{
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = -1
+resource "aws_security_group_rule" "blog_everything_out"{
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = -1
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
